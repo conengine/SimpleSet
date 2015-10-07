ab4e4de82b91889c61dad52ad4337e4d03750884
Satoru Takashimizu <kor19952006@gmail.com>
2015-10-07 14:26:12 +0900

    test

--- SimpleSet/SimpleSet.m
+++ SimpleSet/SimpleSet.m
@@ -15,28 +15,36 @@ abstructSetQ::usage = "";
 Begin["`Private`"]
 (* Implementation of the package *)
 
+(* TODO:Move 'abstructSets' from Module to private.*)
 (* TODO:implement "comprehension" mechanism *)
-(* TODO:Refactor by making immediateQ function. *)
 (* TODO:implement all method of abstruct ver.*)
 (* TODO:implement all method between  abstruct and immediate.*)
 
 (* ::Section:: *)
 (* Constructor *)
 
+setMake // Options = {"abstructSet" -> False}; 
+
 Module[{abstructSets = {Primes, Integers, Rationals, Reals, Complexes}},
-    setMake[arg_] /; MemberQ[abstructSets, arg] := 
-        set@Association["elems" -> arg, "type" -> "abstruct"]
+    setMake[arg_, OptionsPattern[]] /; MemberQ[abstructSets, arg] := 
+        set@Association["elems" -> arg, "type" -> "abstruct"] /; OptionValue@"abstructSet" 
 ]
- 
-setMake[] := setMake@{}
 
-setMake@elems_List := 
- set@Association["elems" -> Union@elems, "type" -> "immediate"]
+(*abstruct set:*)
+setMake[arg_, OptionsPattern[]] := 
+    set@Association["elems" -> arg, "type" -> "abstruct"] /; OptionValue@"abstructSet"
 
-setMake[x_] := x
+setMake[{}, OptionsPattern[]] := 
+    set@Association["elems" -> {}, "type" -> "abstruct"] /; OptionValue@"abstructSet"
+
+
+setMake[elems_List, OptionsPattern[]]  := 
+    set@Association["elems" -> Union@elems, "type" -> "immediate"] /; Not@OptionValue@"abstructSet"
+
+setMake[x_, OptionsPattern[]] /; Not@OptionValue@"abstructSet" := x
 
 (* ::Section:: *)
-(* set data type *)
+(* 'set' data type *)
 
 set[ass_Association]~Format~TraditionalForm := 
  BracketingBar @@ (ass@"elems") /; ass@"type" == "immediate"
@@ -67,14 +75,13 @@ cpoNumbers =(*Flatten@*)
  If[Length@# > 1, 
     Append[#0@Rest@#, First@# \[DirectedEdge](*Rest@*)#(*//
      Thread*)], {}] &@omegaChainNumbers;
-Print@Graph[Thread /@ cpoNumbers // Flatten, VertexLabels -> "Name"]
 
-numberQfunc[Reals] = # \[Element] Reals &;
-numberQfunc[Integers] = Head@SetPrecision[#, \[Infinity]] === Integer &;
-numberQfunc[Primes] = PrimeQ;
-numberQfunc[Complexes] = # \[Element] Complexes &;
-numberQfunc[Rationals] = # \[Element] Rationals &;
-numberQfunc["naturals"] = numberQfunc[Integers]@# \[And] # >= 0 &;
+numberQfunc@Reals = # \[Element] Reals &;
+numberQfunc@Integers = Head@SetPrecision[#, \[Infinity]] === Integer &;
+numberQfunc@Primes = PrimeQ;
+numberQfunc@Complexes = # \[Element] Complexes &;
+numberQfunc@Rationals = # \[Element] Rationals &;
+numberQfunc@"naturals" = numberQfunc[Integers]@# \[And] # >= 0 &;
 
 set /: A_set?immediateSetQ == B_set?immediateSetQ := A@"elems" == B@"elems"
 set /: Equal[A_set?immediateSetQ, B_set?immediateSetQ, Cs__set] := A == B && Equal[B, Cs]
@@ -83,7 +90,7 @@ set /: Normal[A_set?immediateSetQ] := A@"elems"
 
 set /: (Alternatives|List)@B__ \[Element] A_set(*?immediateSetQ*) := 
     AllTrue[{B}, # \[Element] A&]
-set /: b_ \[Element] A_set?immediateSetQ := MemberQ[A, b]
+set /: b_ \[Element] A_set?immediateSetQ := MemberQ[A@"elems", b]
 set /: b_ \[Element] A_set?abstructSetQ /; MemberQ[omegaChainNumbers, A@"elems"] := 
     numberQfunc[A@"elems"]@b
 
@@ -93,15 +100,15 @@ set /: A_set?immediateSetQ \[Intersection] B_set?immediateSetQ :=
  A@"elems" \[Intersection] B@"elems" // setMake
  
 set /: A_set?immediateSetQ \[Union] B_set?immediateSetQ := 
- A@"elems" \[Union] B@"elems" // setMake
+    A@"elems" \[Union] B@"elems" // setMake
 
 set /: A_set?immediateSetQ~Complement~B_set?immediateSetQ := 
- A@"elems"~Complement~B@"elems" // setMake
+    A@"elems"~Complement~B@"elems" // setMake
 
 set /: Length@A_set?immediateSetQ := Length@A@"elems"
 
 set /: Map[f_, A_set?immediateSetQ, levelspec_: {1}] := 
- Map[f, A@"elems", levelspec] // setMake
+    Map[f, A@"elems", levelspec] // setMake
  
 set /: Subsets[A_set?immediateSetQ, args___] := setMake /@ Subsets@A@"elems" // setMake 
 
